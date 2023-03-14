183f2b229d86e6165af493e1bcfefe3dda56fb86
Taeyoung-Park1 <80806455+Taeyoung-Park1@users.noreply.github.com>
2023-03-14 23:18:01 +0900

    version change

--- Assets/02Scripts/Game2/PlayerController.cs
+++ Assets/02Scripts/Game2/PlayerController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    bool isJump = false;
+    bool isTop = false;
+    public float jumpHeight = 0;
+    public float jumpSpeed = 0;
+
+    Vector2 startPosition; //캐릭터 시작위치
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            isJump = true;
+        }
+        if(isJump ) 
+        {
+            if(transform.position.y <= jumpHeight - 0.1f && !isTop) 
+            {
+                transform.position = Vector2.Lerp(transform.position, new Vector2(transform.position.x, jumpHeight), jumpSpeed * Time.deltaTime);
+            }
+            else
+            {
+                isTop = true;
+            }
+        }
     }
 }
