79a39440b766bbcb2abb86696df8870dd6dc8402
Taeyoung-Park1 <80806455+Taeyoung-Park1@users.noreply.github.com>
2023-03-30 23:37:49 +0900

    메인화면_제작(1)

---
+++ Assets/02Scripts/SceneLoadManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadManager : MonoBehaviour
+{
+    public void AvoidScene()
+    {
+        SceneManager.LoadScene("GameScene1");
+    }
+
+    public void JumpScene()
+    {
+        SceneManager.LoadScene("GameScene2");
+    }
+
+    public void PickmeScene()
+    {
+        SceneManager.LoadScene("GameScene3");
+    }
+
+}
