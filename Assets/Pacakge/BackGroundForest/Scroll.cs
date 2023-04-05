62f6d361130e3f7d8bf6c1cca4a35b9b5f8eb4f8
unipuppy81 <113489927+unipuppy81@users.noreply.github.com>
2023-04-05 10:59:53 +0900

    에셋

---
+++ Assets/Pacakge/BackGroundForest/Scroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer scroll;
+    [SerializeField] float ScrollSpeed;
+    Vector2 newOffset;
+
+    void Start()
+    {
+        scroll = gameObject.GetComponent<SpriteRenderer>();
+        Vector2 newOffset = scroll.material.mainTextureOffset;
+    }
+
+    void Update()
+    {
+        Vector2 newOffset = scroll.material.mainTextureOffset;
+        newOffset.Set(newOffset.x + (ScrollSpeed * Time.deltaTime), 0 );
+        scroll.material.mainTextureOffset = newOffset;
+    }
+}
