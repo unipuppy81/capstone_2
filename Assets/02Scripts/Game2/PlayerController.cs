a1649a3c0a3b44d199d54862b0266c96d4b52ff2
Taeyoung-Park1 <taey8780@gmail.com>
2023-04-12 11:42:30 +0900

    GameScene2_매무리(1)

--- Assets/02Scripts/Game2/PlayerController.cs
+++ Assets/02Scripts/Game2/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     bool isTop = false;
     public float jumpHeight = 0;
     public float jumpSpeed = 0;
+    public GameObject Upbtn;
+    public GameObject Downbtn;
 
     Vector2 startPosition; //캐릭터 시작위치
     Animator animator; //애니메이션 변수
@@ -30,22 +32,35 @@ public class PlayerController : MonoBehaviour
         {
             animator.SetBool("RUN", false);
         }
+        UpBtn();
+        DownBtn();
+    }
 
-        if (Input.GetMouseButtonDown(0)&& GameManager.instance.isPlay)
+    public void DownBtn()
+    {
+        if(isJump && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position = startPosition;
+        }
+    }
+    public void UpBtn()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && GameManager.instance.isPlay)
         {
             isJump = true;
             animator.SetBool("JUMP", true);
         }
-        else if(transform.position.y <= startPosition.y)
+        else if (transform.position.y <= startPosition.y)
         {
             isJump = false;
             isTop = false;
             transform.position = startPosition;
             animator.SetBool("JUMP", false);
         }
-        if(isJump ) 
+        if (isJump)
         {
-            if(transform.position.y <= jumpHeight - 0.1f && !isTop) 
+            if (transform.position.y <= jumpHeight - 0.1f && !isTop)
             {
                 transform.position = Vector2.Lerp(transform.position, new Vector2(transform.position.x, jumpHeight), jumpSpeed * Time.deltaTime);
             }
@@ -53,13 +68,12 @@ public class PlayerController : MonoBehaviour
             {
                 isTop = true;
             }
-            if(transform.position.y > startPosition.y && isTop)
+            if (transform.position.y > startPosition.y && isTop)
             {
-                transform.position = Vector2.MoveTowards(transform.position,startPosition,jumpSpeed * Time.deltaTime);
+                transform.position = Vector2.MoveTowards(transform.position, startPosition, jumpSpeed * Time.deltaTime);
             }
         }
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Ob"))
