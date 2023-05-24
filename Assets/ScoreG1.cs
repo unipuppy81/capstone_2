c673e9d81daf9bfdc694e0c724c52c83c3a117f8
unipuppy81 <113489927+unipuppy81@users.noreply.github.com>
2023-05-24 11:07:14 +0900

    버그 수정

--- Assets/ScoreG1.cs
+++ Assets/ScoreG1.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class ScoreG1 : MonoBehaviour
 {
     MoveG1 moveG1;
+    SceneManagerG1 smg;
     public TMP_Text GameScore;
     public TextMeshProUGUI GameScore2;
     public TextMeshProUGUI BestScoreText;
@@ -17,6 +18,7 @@ public class ScoreG1 : MonoBehaviour
     void Awake()
     {
         moveG1 = GameObject.Find("Player").GetComponent<MoveG1>();
+        smg = GameObject.Find("SceneManager").GetComponent<SceneManagerG1>();
         GameScore = GetComponent<TextMeshProUGUI>();
         GameScore2 = GetComponent<TextMeshProUGUI>();
         BestScoreText = GetComponent<TextMeshProUGUI>();
@@ -30,14 +32,16 @@ public class ScoreG1 : MonoBehaviour
     }
     void Update()
     {
-        score1 += Time.deltaTime;
-        
-        if(a <= 1)
+        if (smg.isStart) { 
+            score1 += Time.deltaTime;
+        }
+
+        if (a <= 5)
         {
-            score1 -= 0.01f;
+            //score1 -= 0.01f;
             a++;
         }
-        GameScore.text = "" + score1.ToString("N2");
+        GameScore.text = "Time : " + score1.ToString("N2");
         //score1 = moveG1.gameTime;
         //GameScore.text = score1.ToString();
         //GameScore2.text = score1.ToString();
