580cf0f95873d113535c738f7d4840e5610265ff
Taeyoung-Park1 <80806455+Taeyoung-Park1@users.noreply.github.com>
2023-05-29 23:16:36 +0900

    화면 비율 18/9 고정, (canvas,카메라 수정)

---
+++ Assets/02Scripts/GameManager/ScreenSize.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSize : MonoBehaviour
+{
+    private void Awake()
+    {
+        Camera cam = GetComponent<Camera>();
+
+        // 카메라 컴포넌트의 Viewport Rect
+        Rect rt = cam.rect;
+
+        // 현재 세로 모드 9:16, 반대로 하고 싶으면 16:9를 입력.
+        float scale_height = ((float)Screen.width / Screen.height) / ((float)9 / 18); // (가로 / 세로)
+        float scale_width = 1f / scale_height;
+
+        if (scale_height < 1)
+        {
+            rt.height = scale_height;
+            rt.y = (1f - scale_height) / 2f;
+        }
+        else
+        {
+            rt.width = scale_width;
+            rt.x = (1f - scale_width) / 2f;
+        }
+
+        cam.rect = rt;
+    }
+}
